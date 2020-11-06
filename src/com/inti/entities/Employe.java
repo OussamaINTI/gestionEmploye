4dafa372d12fbec1511e66dfee96cbb0d5a2503d
OussamaINTI <ayari_oussama26@hotmail.fr>
2020-11-06 04:07:24 +0100

    first commit

---
+++ src/com/inti/entities/Employe.java
@@ -0,0 +1,110 @@
+package com.inti.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Employe implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idEmploye;
+	private String nomEmploye;
+	private String prenomEmploye;
+	@Temporal(TemporalType.DATE)
+	private Date dateNaissance;
+	private String username;
+	private String password;
+	private String sexe;
+	private String email;
+	@ManyToOne
+	private Departement departement;
+
+	public Employe() {
+	}
+
+	public Long getIdEmploye() {
+		return idEmploye;
+	}
+
+	public void setIdEmploye(Long idEmploye) {
+		this.idEmploye = idEmploye;
+	}
+
+	public String getNomEmploye() {
+		return nomEmploye;
+	}
+
+	public void setNomEmploye(String nomEmploye) {
+		this.nomEmploye = nomEmploye;
+	}
+
+	public String getPrenomEmploye() {
+		return prenomEmploye;
+	}
+
+	public void setPrenomEmploye(String prenomEmploye) {
+		this.prenomEmploye = prenomEmploye;
+	}
+
+	public Date getDateNaissance() {
+		return dateNaissance;
+	}
+
+	public void setDateNaissance(Date dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getSexe() {
+		return sexe;
+	}
+
+	public void setSexe(String sexe) {
+		this.sexe = sexe;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Departement getDepartement() {
+		return departement;
+	}
+
+	public void setDepartement(Departement departement) {
+		this.departement = departement;
+	}
+
+}
